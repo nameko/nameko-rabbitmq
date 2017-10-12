faf17808c4df672b07d9fdf5adad40e08fc85674
Matt Bennett <matt@bennett.name>
2017-10-12 03:49:50 +0100

    wait for management api to come up before returning

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,3 +4,11 @@ set -e
 cp /srv/ssl/* /mnt/certs
 
 exec "$@"
+
+still_booting=1
+while [ "$still_booting" -ne 0 ]
+do
+  curl http://localhost:15672/api/overview
+  still_booting=$?
+  sleep 1
+done
